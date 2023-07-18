5f13b852c76968ddf69cff7aa676c076b4adfee4
Florian Louche <florian.louche@boulangerie-ange.fr>
2023-07-18 11:50:14 +0200

    Env

--- public/index.php
+++ public/index.php
@@ -1,10 +1,6 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
